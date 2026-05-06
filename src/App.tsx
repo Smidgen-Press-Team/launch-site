1d7004c64e5bd8b478fdf4ebcb99b188c47dbdb2
michael-masarik <masarikfamilymichael@gmail.com>
2026-05-06 16:24:24 -0500

    Added Name input to cart

--- src/App.tsx
+++ src/App.tsx
@@ -58,6 +58,8 @@ export default function App() {
         checkoutUrl={checkoutUrl}
         status={status}
         attributes={attributes}
+        acknowledgmentName={acknowledgmentName}
+        setAcknowledgmentName={setAcknowledgmentName}
       />
 
       <Hero
